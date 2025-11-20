274c106b3bb221cd7a0b771cb9f8e181d3081f7d
NEELI HARIKA <neelih19@gmail.com>
2025-11-20 15:00:22 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -7,12 +7,12 @@ always@(*) begin
 case(sel)
     3'b000: result = a+b;
     3'b001: result = a-b;
-    3'b010: result = a*b;
+    3'b010: result = a>>1;
     3'b011: result= a&b;
     3'b100: result = a|b;
     3'b101: result = ~a;
     3'b110: result = a^b;
-    3'b111: result = ~(a^b);
+    3'b111: result = b<<1;
   endcase 
  end
 endmodule
